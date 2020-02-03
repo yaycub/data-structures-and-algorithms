1ce05ecf6c8df48d636ee9d82aba104728872f36
Jacob Harrington <jharrington4@gmail.com>
2020-02-03 15:37:23 -0800

    attempted code challenge

--- tree/tree.js
+++ tree/tree.js
@@ -17,8 +17,19 @@ class BinarySearchTree{
     if(this.root === null){
       this.root = newNode;
     } else {
-      
+      this.insertNode(this.root, newNode);
     }
+  }
+
+  insertNode(node, newNode){  
+    if(newNode.data < node.data){ 
+      if(node.left === null) node.left = newNode; 
+      else this.insertNode(node.left, newNode);  
+      
+    } else { 
+      if(node.right === null) node.right = newNode; 
+      else this.insertNode(node.right, newNode); 
+    } 
   }
 
   preOrder() {
